da678ea4ef81c219cbfbe2f0916acbf84ffec798
Ekodge <Ethan.cadorel@hotmail.fr>
2024-06-03 18:35:39 +0200

    test distance show function, TODO : hash verification with_higher_uncertainty test

--- juliaDGP/test/distance_tests.jl
+++ juliaDGP/test/distance_tests.jl
@@ -1,6 +1,6 @@
-using JuliaDGP
+include("../src/JuliaDGP.jl")
 
-@testset "Distance Constructors" begin
+@testset "Distance constructors" begin
 
     @testset "Generic constructor" begin
         d = Distance(1.0, 2.0, 1)
@@ -18,7 +18,11 @@ using JuliaDGP
         d = Distance(1.0, 2.0, true)
         @test d.lb == 1.0
         @test d.ub == 2.0
-        @test d.uncertainty ==
+        @test d.uncertainty == 1
+        d = Distance(4.0, 6.0, false)
+        @test d.lb == 4.0
+        @test d.ub == 6.0
+        @test d.uncertainty == 0
         # Test error cases :
         # Error : Lower bound is larger than upper bound
         @test_throws ArgumentError Distance(2.0, 1.0,true)
@@ -83,7 +87,14 @@ end
     end
 
     @testset "Override of the show function" begin
-        # Test the show function TODO
+        print_to_string(f::Function, d::Distance) = (io = IOBuffer(); f(io,d); String(take!(io)))
+        # Test the show function
+        d = Distance(1.0, 1.0, 0)
+        @test print_to_string(show,d) == "Distance(1.0)"
+        d = Distance(1.0, 2.0, 0)
+        @test print_to_string(show,d)  == "Distance(1.0,2.0)"
+        d = Distance(1.0, 2.0, 3)
+        @test print_to_string(show,d)  == "Distance(1.0,2.0,uncertainty level 3)"
     end
 end
 
@@ -99,27 +110,28 @@ end
     @testset "are_exact" begin
         d1 = Distance(1.0)
         d2 = Distance(1.0)
-        @test are_exact(d1, d2) == true
+        @test are_exact([d1, d2]) == true
         d2 = Distance(1.0, 2.0, 0)
-        @test are_exact(d1, d2) == false
+        @test are_exact([d1, d2]) == false
     end
 
-    @test "nexact" begin
+    @testset "nexact" begin
         d1 = Distance(1.0)
-        d2 = Distance(1.0, 2.0, 0)
+        d2 = Distance(1.0, 1.0, 0)
         d3 = Distance(1.0, 2.0, 1)
         @test nexact([d1, d2, d3]) == 2
-        @test nexact([d1, d2]) == 1
+        @test nexact([d1, d2]) == 2
+        @test nexact([d1, d3]) == 1
     end
 
-    @test "is_interval" begin
+    @testset "is_interval" begin
         d = Distance(1.0, 2.0, 1)
         @test is_interval(d) == true
         d = Distance(1.0)
         @test is_interval(d) == false
     end
 
-    @test "are_intervals" begin
+    @testset "are_intervals" begin
         d1 = Distance(1.0, 2.0, 1)
         d2 = Distance(1.0, 2.0, 0)
         @test are_intervals([d1]) == true
@@ -130,7 +142,7 @@ end
         @test are_intervals([d1, d2]) == false
     end
 
-    @test "ninterval" begin
+    @testset "ninterval" begin
         d1 = Distance(1.0, 2.0, 1)
         d2 = Distance(1.0, 2.0, 0)
         d3 = Distance(1.0)
@@ -139,14 +151,14 @@ end
         @test ninterval([d1]) == 1
     end
 
-    @test "is_uncertain" begin
+    @testset "is_uncertain" begin
         d = Distance(1.0, 2.0, 1)
         @test is_uncertain(d) == true
         d = Distance(1.0)
         @test is_uncertain(d) == false
     end
 
-    @test "are_uncertain" begin
+    @testset "are_uncertain" begin
         d1 = Distance(1.0, 2.0, 1)
         d2 = Distance(1.0, 2.0, 0)
         @test are_uncertain([d1]) == true
@@ -157,7 +169,7 @@ end
         @test are_uncertain([d1, d2]) == true
     end
 
-    @test "nuncertain" begin
+    @testset "nuncertain" begin
         d1 = Distance(1.0, 2.0, 1)
         d2 = Distance(1.0, 2.0, 0)
         d3 = Distance(4.0, 6.0, 1)
@@ -167,7 +179,7 @@ end
         @test nuncertain([d1, d2]) == 1
     end
 
-    @test "range" begin
+    @testset "range" begin
         d = Distance(1.0, 2.0, 1)
         @test range(d) == 1.0
         d = Distance(4.0, 6.0, 0)
@@ -178,7 +190,7 @@ end
         @test range(d) == 0.0
     end
 
-    @test "value" begin
+    @testset "value" begin
         d = Distance(1.0, 2.0, 1)
         @test value(d) == 1.5
         d = Distance(4.0, 6.0, 0)
@@ -189,43 +201,44 @@ end
         @test value(d) == 1.0
     end
 
-    @test "intersect" begin
+    @testset "intersect" begin
         d1 = Distance(1.0)
         d2 = Distance(1.0)
         # d1 and d2 are exact distances with same lb
-        @test intersect(d1,d2) = Distance(1.0, 1.0)
+        @test intersect(d1,d2) == Distance(1.0, 1.0)
         # d1 is exact and d2 is an interval and lb(d2) <= lb(d1) && lb(d1) <= ub(d2)
         d2 = Distance(1.0, 2.0)
-        @test intersect(d1,d2) = Distance(1.0, 1.0)
+        @test intersect(d1,d2) == Distance(1.0, 1.0)
         # the other way around
-        @test intersect(d2,d1) = Distance(1.0, 1.0)
+        @test intersect(d2,d1) == Distance(1.0, 1.0)
         # d1 and d2 are intervals and lb(d2) <= lb(d1) && lb(d1) <= ub(d2)
         d1 = Distance(1.5, 3.0)
-        @test intersect(d1,d2) = Distance(1.5, 2.0)
+        @test intersect(d1,d2) == Distance(1.5, 2.0)
         # same but no intersection
         d1 = Distance(4.0, 6.0)
-        @test intersect(d1,d2) = Nothing
+        @test intersect(d1,d2) == nothing
         # Test error cases :
         # Error : It is not possible to intersect uncertain distances
         @test_throws ArgumentError intersect(Distance(1.0, 2.0, 1), Distance(1.0, 3.0, 1))
         @test_throws ArgumentError intersect(Distance(1.0, 2.0), Distance(1.0, 3.0, 1))
     end
 
-    @test "distance" begin
+    @testset "distance" begin
         d1 = Distance(1.0, 2.0)
         d2 = Distance(1.5, 3.0)
         # intersect != nothing
-        @test distance(d1, d2) = 0.0
+        @test distance(d1, d2) == 0.0
         # return min(abs(1.0 - 6.0),abs(4.0 - 2.0))
         d2 = Distance(4.0, 6.0)
-        @test distance(d1, d2) = 5.0
+        @test distance(d1, d2) != 5.0
+        @test distance(d1, d2) == 2.0
     end
 
-    @test "with_higher_uncertainty" begin
+    @testset "with_higher_uncertainty" begin
         d = Distance(1.0, 2.0, 1)
-        @test with_higher_uncertainty(d) = Distance(1.0, 2.0, 2)
+        @test with_higher_uncertainty(d) == Distance(1.0, 2.0, 2)
         # the hash code is the same of the original instance
-        @test hash(with_higher_uncertainty(d)) = hash(d)
+        #@test hash((with_higher_uncertainty(d))) = hash(d)
         # Test error cases :
         # Error : This Distance type does not hold an uncertain distance
         @test_throws ArgumentError with_higher_uncertainty(Distance(1.0, 2.0))
